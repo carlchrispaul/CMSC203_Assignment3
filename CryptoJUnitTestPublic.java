56c9df47c7014492f1881c5b38dceaf04685faf3
carlchrispaul <72216825+carlchrispaul@users.noreply.github.com>
2021-10-14 22:11:45 -0400

    Update CryptoJUnitTestPublic.java

--- CryptoJUnitTestPublic.java
+++ CryptoJUnitTestPublic.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
@@ -18,17 +19,30 @@ public class CryptoJUnitTestPublic {
 	@Test
 	public void testStringInBounds() {
 		assertTrue(CryptoManager.stringInBounds("THIS TEST SHOULD SUCCEED"));
+		assertFalse(CryptoManager.stringInBounds("TEST SHOULD FAIL BECAUSE ~ IS OUTSIDE THE RANGE"));
+		assertFalse(CryptoManager.stringInBounds("This test should fail because of lowercase letters"));
 	}
 
 	@Test
 	public void testEncryptCaesar() {
 		assertEquals("IFMMP!XPSME", CryptoManager.encryptCaesar("HELLO WORLD", 1));
+		assertEquals("WKLV#LV#DQRWKHU#WHVW",CryptoManager.encryptCaesar("THIS IS ANOTHER TEST", 3));
 	}
 	
 	@Test
 	public void testDecryptCaesar() {
 		assertEquals("HELLO WORLD", CryptoManager.decryptCaesar("IFMMP!XPSME", 1));
+		assertEquals("THIS IS ANOTHER TEST", CryptoManager.decryptCaesar("WKLV#LV#DQRWKHU#WHVW", 3));
 	}
-
-
+	
+	@Test
+	public void testEncryptBellaso() {
+		assertEquals("WU\\VR9F#N!RF88U-'HED", CryptoManager.encryptBellaso("THIS IS ANOTHER TEST", "CMSC203"));
+	}
+	
+	@Test
+	public void testDecryptBellaso() {
+		assertEquals("THIS IS ANOTHER TEST", CryptoManager.decryptBellaso("WU\\VR9F#N!RF88U-'HED", "CMSC203"));
+	}
+	
 }
